866275fbfa1ef749682a0b3d2264fb3cdb3b0eda
Vivek Krishnakumar <vivekkrishnakumar@gmail.com>
2015-04-16 14:34:46 -0400

    + Allow postMessage event to originate from araport prod and dev

--- public/javascripts/listeners.js
+++ public/javascripts/listeners.js
@@ -1,8 +1,9 @@
 function listener (event) {
-  if ( event.origin !== "https://www.araport.org" )
-    return
-
-  var apiToken = document.getElementById("api-token");
-  apiToken.value = event.data;
-  angular.element(apiToken).triggerHandler('input');
+  if ( event.origin === "https://www.araport.org" || event.origin === "https://araport-dev.tacc.utexas.edu" ) {
+    var apiToken = document.getElementById("api-token");
+    apiToken.value = event.data;
+    angular.element(apiToken).triggerHandler('input');
+  } else {
+    return;
+  }
 }
